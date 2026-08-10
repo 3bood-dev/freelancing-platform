a4f4eb6b34c54dab037a80b43d82193c16f00fe2
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-08-10 20:56:53 +0300

    the intial of the project

---
+++ src/main.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract FreelancingPlatform{
+    ////////State variable////////
+    uint public constant REPUTATION = 1;
+    uint256 public listingFees = 0.001 ether;
+    address public owner ;
+    uint256 public totalJobs;
+    ////////Structs////////
+    struct Job{
+        address client;
+        address freelancer;
+        uint256 bounty;
+        string title;
+        string description;
+        JobStatus status;
+    }
+ 
+    ////////Mapping////////
+    mapping(uint256 => Job) public jobs;
+    mapping(address => uint256) public reputations;
+    ////////Enum////////
+    enum JobStatus{
+        Open,
+        Completed,
+        Pending,
+        Cancelled,
+        Taken
+    }
+    ////////Event////////
+    
+    event jobCreated(uint256 indexed jobID, address indexed client , uint256 bounty);
+    event jobTaken(uint256 indexed jobID, address freelancer);
+    event jobComplited(uint256 indexed jobID, address freelancer);
+    ////////Errors////////
+    error insufficientFunds();
+    ////////Modifier////////
+    modifier onlyOwner(){
+        require(msg.sender == owner , "your not the owner of this contract");
+        _;
+    }
+    ////////Functions////////
+    constructor(){
+        owner = msg.sender;
+    }
+    }
