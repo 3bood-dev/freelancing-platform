79971b957c243f6e96ab1f482b5ee1b8228912e3
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-08-11 19:02:19 +0300

    adding getter getBalance function

--- src/main.sol
+++ src/main.sol
@@ -90,4 +90,7 @@ contract FreelancingPlatform{
      function listtingFees(uint256 _newValue)external onlyOwner{
         listingFees = _newValue;
     }
+    function getBalance() external view returns(uint256){
+        return address(this).balance;
     }
+}
